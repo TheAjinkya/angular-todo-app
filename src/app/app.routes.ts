5cae0a1dece24eef5f915162a383b98c5dd9d04e
Ajinkya Chanshetty <ajinkya.chanshetty@gmail.com>
2025-09-20 01:49:37 +0100

    updated components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { Task } from './task/task';
+import { Dashboard } from './dashboard/dashboard';
+import { ReservationForm } from './hotel-reservation/reservation-form/reservation-form';
+import { ReservationList } from './hotel-reservation/reservation-list/reservation-list';
 
 export const routes: Routes = [
-    { path: '', component: Task }
+    { path: '', component: Dashboard },
+    { path: 'task', component: Task },
+    { path: 'new', component: ReservationForm },
+    { path: 'list', component: ReservationList }
 ];
---
+++ src/app/hotel-reservation/reservation/reservation.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-reservation',
+  imports: [RouterLink, RouterOutlet],
+  templateUrl: './reservation.html',
+  styleUrl: './reservation.scss'
+})
+export class Reservation {
+
+}
